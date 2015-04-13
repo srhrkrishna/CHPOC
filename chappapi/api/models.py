75de117ed501496e4cc2a4d5ca082d4035fa8aa5
Sree Hari Krishna <srhrkrishna@gmail.com>
2015-04-13 16:15:47 +0530

    Implemented thumbnail in header

--- chappapi/api/models.py
+++ chappapi/api/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Video(models.Model):
-    fileData = models.FileField(upload_to='documents/%Y/%m/%d')
-    created = models.DateTimeField(auto_now_add=True)
