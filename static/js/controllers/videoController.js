72aabdffa0539e8778e15f6f1fb7dd026d55a535
Sree Hari Krishna <srhrkrishna@gmail.com>
2015-05-26 08:01:49 +0530

    Updated video playback functionality

--- static/js/controllers/videoController.js
+++ static/js/controllers/videoController.js
@@ -13,19 +13,32 @@ openstackApp.controller('videoController', ['$scope', '$http', '$modalInstance',
             $http.get(url).
             success(function(data, status) {
                 video_tag = document.getElementById('videoPlr');
-                video_tag.src = data;
-                // video_tag.load();
+                source_tag = document.getElementById('mp4Source');
+                source_tag.setAttribute('src', data)
+                // video_tag.src = data;
+                video_tag.load();
+
+                video_tag.addEventListener('canplaythrough', function() {
+                    video_tag = document.getElementById('videoPlr');
+                    video_tag.play();
+                }, false);
+
                 // video_tag.play();
                 // App_Constants.videourl = data;
             }).error(function(data, status) {
-        		
+
             });
         }
     }
 ]);
 
-openstackApp.filter('trusted', ['$sce', function ($sce) {
-    return function(url) {
-        return $sce.trustAsResourceUrl(url);
-    };
-}])
+// openstackApp.filter('trusted', ['$sce', function ($sce) {
+//     return function(url) {
+//         return $sce.trustAsResourceUrl(url);
+//     };
+// }]);
+
+// video_tag = document.getElementById('videoPlr');
+// video_tag.on('loadeddata', function(event) {
+//     video_tag.play();
+// });
