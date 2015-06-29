c4787efc4eef17b7f1139e1ffcaabf876d3c0c0e
Sree Hari Krishna <srhrkrishna@gmail.com>
2015-06-29 11:16:23 +0530

    Updated UI.

--- static/js/services/loginService.js
+++ static/js/services/loginService.js
@@ -17,6 +17,53 @@ openstackApp.service('loginService', ['$http', function ($http) {
 
 }]);
 
+openstackApp.service('jsonService', ['$http', function ($http) {
+
+    var url = App_Constants.url + '/list/?' + new Date(), videoSrvc = {};
+
+    videoSrvc.login = function(success, error) {
+        $http.get(url).
+            success(function(data, status, headers, config) {
+                success(data);
+            }).
+            error(function(data, status, headers, config) {
+                alert("error");
+            });
+    }
+    return videoSrvc;
+
+}]);
+
+
+openstackApp.service('jsonFolderService', ['$http', function ($http) {
+
+
+
+
+    var url = App_Constants.url + '/list/?' + new Date() , folderVideoSrvc = {};
+
+    folderVideoSrvc.getFolderVideos = function(success,folderName, error) {
+
+
+        App_Constants.folderName = folderName;
+
+
+        $http.get(url).
+            success(function(data, status, headers, config) {
+                success(data);
+            }).
+            error(function(data, status, headers, config) {
+                alert("error");
+                alert(data);
+                alert(status);
+                alert(headers);
+
+            });
+    }
+    return folderVideoSrvc;
+
+}]);
+
 openstackApp.factory('authTokenFactory', ['$q', function ($q) {
 
     var token = 'token';
@@ -24,6 +71,9 @@ openstackApp.factory('authTokenFactory', ['$q', function ($q) {
     return {
         request: function (config) {
         	config.headers['x-a12n'] = App_Constants.auth;
+            if(App_Constants.folderName.length > 0){
+                config.headers['x-pseudo-folder'] = App_Constants.folderName;
+            }
             return config || $q.when(config);
         },
         response: function (response) {
